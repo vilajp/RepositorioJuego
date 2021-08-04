9aeb20ba03a14a067b8b682de3087f23aef295da
vilajp <vilajp@gmail.com>
2021-08-04 17:13:29 -0300

    primer commit juego grupo 8

---
+++ juego/juego/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'BDJUEGO',
+        'Truested_Connection': 'yes',
+        'HOST': 'localhost\\SQLEXPRESS',
+        'OPTIONS':{
+            'driver':'SQL Server Native Client 11.0',
+        }
+    }
+}
