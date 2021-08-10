78c7888df48a0acdbc6e13496a3d00afe0faa016
vilajp <vilajp@gmail.com>
2021-08-10 16:39:05 -0300

    se Agrego app usuario y modificaciones

--- juego/juego/settings/base.py
+++ juego/juego/settings/base.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+AUTH_USER_MODEL = 'usuarios.Usuario'
+
+# LOGIN_REDIRECT_URL = reverse_lazy('home')
+# LOGOUT_REDIRECT_URL = reverse_lazy('login')
+# LOGIN_URL_URL = reverse_lazy('login')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.usuarios',
 ]
 
 MIDDLEWARE = [
