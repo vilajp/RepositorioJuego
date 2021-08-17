31ce9895db23f9f327ae4ed0e3e9c2cbd37490c0
nicolasaguirre93 <nicolasivanaguirre@gmail.com>
2021-08-17 17:20:44 -0300

    se actualizó partida.html y view.py

--- juego/apps/partida/views.py
+++ juego/apps/partida/views.py
@@ -3,17 +3,20 @@ from apps.partida.models import Preguntas,Respuestas, Respuesta_Incorrecta
 import random
 # Create your views here.
 #Esta funcion carga una pregunta aleatoria
-def comienzo(request,respuesta=""):
-    if respuesta=="":
-        lista_preguntas = Preguntas.objects.all()
-        cantidad = len(lista_preguntas)
-        una_id = random.randint(1,cantidad)
-        una_pregunta = Preguntas.objects.get(id=una_id)
+def comienzo(request):
+    lista_preguntas = Preguntas.objects.all()
+    cantidad = len(lista_preguntas)
+    una_id = random.randint(1,cantidad)
+    una_pregunta = Preguntas.objects.get(id=una_id)
+    if request.method == 'GET':
         context = {"pregunta":una_pregunta,"respuesta":una_pregunta.respuesta_correcta,"incorrecta":una_pregunta.respuesta_erronea}
-        return render(request,"prueba.html",context)
+        return render(request,"partida.html",context)
 
-    else:
-        print(respuesta)
+    elif request.method == 'POST':
+        if request.POST.get("respuesta") == str(una_pregunta.respuesta_correcta):
+            return HttpResponse("Felicidades!!!")
+        else:
+            return HttpResponse("Fallaste!!!")
 
     
 
