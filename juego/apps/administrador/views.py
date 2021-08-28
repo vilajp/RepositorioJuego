446d48efb4bd6145a08ff027020e8c80026cdf66
vilajp <vilajp@gmail.com>
2021-08-28 10:24:38 -0300

    sigo creando version del administrador para cargar preguntas

--- juego/apps/administrador/views.py
+++ juego/apps/administrador/views.py
@@ -19,16 +19,21 @@ def admin(request):
         respuestas = request.POST.getlist('respuestas')
 
         objeto_categoria = Categoria.objects.get(id = int(categoria_id))
-
         nueva_pregunta = Pregunta(texto = pregunta, categoria = objeto_categoria)
-        nueva_pregunta.save()
-
+        
+        
+        alguna_correcta = False
         for cada_respuesta in respuestas:
-        if cada_respuesta[-1]=="*":
-                nueva_respuesta = Respuesta(texto =cada_respuesta[2:-3].strip(), es_correcta =True, pregunta = todas_las_preguntas)
+
+            print(cada_respuesta)
+            if cada_respuesta[0].strip("\r\n")=="*":
+                alguna_correcta = True
+                nueva_respuesta = Respuesta(texto =cada_respuesta[1:].strip("\r\n"), es_correcta =True, pregunta = nueva_pregunta)
             else:
-                nueva_respuesta = Respuesta(texto =cada_respuesta[2:].strip(), es_correcta =False, pregunta = todas_las_preguntas)
-            
+                nueva_respuesta = Respuesta(texto =cada_respuesta.strip("\r\n"), es_correcta =False, pregunta = nueva_pregunta)
+
+        if alguna_correcta:
+            nueva_pregunta.save()
             nueva_respuesta.save()
 
         return redirect('administrador')
